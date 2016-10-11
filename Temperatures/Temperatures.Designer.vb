028b0fd117c9ad7239467ba25a1c608c2e212f74
Boris Lachev <boris.lachevbg00@gmail.com>
2016-10-11 11:06:00 +0300

    Intial and final commit.

---
+++ Temperatures/Temperatures.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Temperatures
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.celsiusLabel = New System.Windows.Forms.Label()
+        Me.fahrenheitLabel = New System.Windows.Forms.Label()
+        Me.celsiusTextBox = New System.Windows.Forms.TextBox()
+        Me.fahrenheitTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'celsiusLabel
+        '
+        Me.celsiusLabel.AutoSize = True
+        Me.celsiusLabel.Location = New System.Drawing.Point(38, 23)
+        Me.celsiusLabel.Name = "celsiusLabel"
+        Me.celsiusLabel.Size = New System.Drawing.Size(43, 13)
+        Me.celsiusLabel.TabIndex = 0
+        Me.celsiusLabel.Text = "Celsius:"
+        '
+        'fahrenheitLabel
+        '
+        Me.fahrenheitLabel.AutoSize = True
+        Me.fahrenheitLabel.Location = New System.Drawing.Point(21, 62)
+        Me.fahrenheitLabel.Name = "fahrenheitLabel"
+        Me.fahrenheitLabel.Size = New System.Drawing.Size(60, 13)
+        Me.fahrenheitLabel.TabIndex = 1
+        Me.fahrenheitLabel.Text = "Fahrenheit:"
+        '
+        'celsiusTextBox
+        '
+        Me.celsiusTextBox.Location = New System.Drawing.Point(88, 23)
+        Me.celsiusTextBox.Name = "celsiusTextBox"
+        Me.celsiusTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.celsiusTextBox.TabIndex = 2
+        '
+        'fahrenheitTextBox
+        '
+        Me.fahrenheitTextBox.Location = New System.Drawing.Point(88, 62)
+        Me.fahrenheitTextBox.Name = "fahrenheitTextBox"
+        Me.fahrenheitTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.fahrenheitTextBox.TabIndex = 3
+        '
+        'Temperatures
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(224, 122)
+        Me.Controls.Add(Me.fahrenheitTextBox)
+        Me.Controls.Add(Me.celsiusTextBox)
+        Me.Controls.Add(Me.fahrenheitLabel)
+        Me.Controls.Add(Me.celsiusLabel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(230, 150)
+        Me.MinimumSize = New System.Drawing.Size(230, 150)
+        Me.Name = "Temperatures"
+        Me.Text = "Temperatures"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents celsiusLabel As System.Windows.Forms.Label
+    Friend WithEvents fahrenheitLabel As System.Windows.Forms.Label
+    Friend WithEvents celsiusTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents fahrenheitTextBox As System.Windows.Forms.TextBox
+
+End Class
---
+++ Temperatures/Temperatures.vb
@@ -0,0 +1,37 @@
+﻿Public Class Temperatures
+
+    Private Sub celsiusTextBoxKeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles celsiusTextBox.KeyUp
+        Dim isDigit As Boolean = Char.IsDigit(ChrW(e.KeyValue))
+        Dim isKeypadNum As Boolean = e.KeyCode >= Keys.NumPad0 And e.KeyCode <= Keys.NumPad9
+        Dim isUtility As Boolean = (e.KeyCode = Keys.Back Or e.KeyCode = Keys.Enter Or e.KeyCode = Keys.Escape)
+        If Not (isDigit Or isKeypadNum Or isUtility) Then
+            MessageBox.Show("That's not numeric!")
+            If celsiusTextBox.Text.Length > 0 Then
+                celsiusTextBox.Text = celsiusTextBox.Text.Remove(celsiusTextBox.Text.Length - 1)
+            End If
+        End If
+        Dim fahrenheitResult As Int64
+        If celsiusTextBox.Text.Length > 0 Then
+            fahrenheitResult = Math.Floor(Math.Floor(Convert.ToDouble(celsiusTextBox.Text) * 1.8) + 32)
+        End If
+        fahrenheitTextBox.Text = fahrenheitResult
+
+    End Sub
+    Private Sub fahrenheitTextBoxKeyUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles fahrenheitTextBox.KeyUp
+        Dim isDigit As Boolean = Char.IsDigit(ChrW(e.KeyValue))
+        Dim isKeypadNum As Boolean = e.KeyCode >= Keys.NumPad0 And e.KeyCode <= Keys.NumPad9
+        Dim isUtility As Boolean = (e.KeyCode = Keys.Back Or e.KeyCode = Keys.Enter Or e.KeyCode = Keys.Escape)
+        If Not (isDigit Or isKeypadNum Or isUtility) Then
+            MessageBox.Show("That's not numeric!")
+            If fahrenheitTextBox.Text.Length > 0 Then
+                fahrenheitTextBox.Text = fahrenheitTextBox.Text.Remove(fahrenheitTextBox.Text.Length - 1)
+            End If
+            fahrenheitTextBox.Text = fahrenheitTextBox.Text.Remove(fahrenheitTextBox.Text.Length - 1)
+        End If
+        Dim celsiusResult As Int64
+        If fahrenheitTextBox.Text.Length > 0 Then
+            celsiusResult = Math.Floor(((Convert.ToInt64(fahrenheitTextBox.Text) - 32) * 5) / 9)
+        End If
+        celsiusTextBox.Text = celsiusResult
+    End Sub
+End Class
